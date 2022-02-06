ca4f96d5d67f9b977375e61d75acfde3715f28e5
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-02-05 20:06:41 -0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,15 +6,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemsInCart}) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon'/>
-    <span className="item-count">2</span>
+    <span className="item-count">{itemsInCart}</span>
   </div>
 );
+
+const mapStateToProps = ({cart}) => ({
+  itemsInCart: cart.cartItems.length
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
