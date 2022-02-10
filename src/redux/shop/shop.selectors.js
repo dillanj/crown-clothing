033a186b13be9bc7184d1ccdaf39b1a1f3262103
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-02-09 22:57:32 -0700

    added directory and shop data intio its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize'; // used to memoize a selector with a dynamic param
+
+const selectShop = state => state.shop;
+
+
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = memoize((collectionUrlParam) => 
+// returning the createSelecot funciton (currying) - returning a function within a function
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  )
+);
